711d840884ecd317d59b762dd156b9101945a460
JimmyBeton7 <mateusz.durka77@gmail.com>
2024-06-29 12:03:12 +0200

    Dodano nowe flagi do skryptu

--- skrypt.sh
+++ skrypt.sh
@@ -2,19 +2,33 @@
 
 function show_help() {
     echo "Available options:"
-    echo "--date, -d      Display today's date"
-    echo "--logs, -l [N]  Create N log files (default 100)"
-    echo "--help, -h      Display this help message"
+    echo "--date, -d          Display today's date"
+    echo "--logs, -l [N]      Create N log files (default 100)"
+    echo "--error, -e [N]     Create N error files (default 100)"
+    echo "--help, -h          Display this help message"
+    echo "--init              Clone the repository and set PATH"
+}
+
+function create_files() {
+    prefix=$1
+    num_files=${2:-100}
+    for ((i=1; i<=num_files; i++)); do
+        dirname="${prefix}${i}"
+        mkdir -p ${dirname}
+        filename="${dirname}/${prefix}${i}.txt"
+        echo "Filename: ${filename}, Script: skrypt.sh, Date: $(date)" > ${filename}
+    done
 }
 
 if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
 elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
-    num_files=${2:-100}
-    for ((i=1; i<=num_files; i++)); do
-        filename="log${i}.txt"
-        echo "Filename: ${filename}, Script: skrypt.sh, Date: $(date)" > ${filename}
-    done
+    create_files "log" $2
+elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
+    create_files "error" $2
+elif [[ "$1" == "--init" ]]; then
+    git clone <URL_REPOZYTORIUM> cloned_repo
+    export PATH=$PATH:$(pwd)/cloned_repo
 elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     show_help
 else
